1a369d8b1f6295bc7fb47093df3540047a5421ca
ARMANDO MARTINEZ SIMON <Armando.Martinez@alu.uclm.es>
2022-06-13 18:51:32 +0200

    email.js

--- servidor/email.js
+++ servidor/email.js
@@ -1,28 +1,30 @@
-//var sendgrid = require("sendgrid")("xxxxx","xxxxx");
+var sendgrid = require("sengrid")("clavexxx","paswordxxx");
 
 const sgMail = require('@sendgrid/mail')
-sgMail.setApiKey("xxxxxxxxx")//process.env.SENDGRID_API_KEY)
+sgMail.setApiKey("XXXXXXXXXXXXXXX")//process.env.SENDGRID_API_KEY
 
-var url="http://127.0.0.1:5000/";
-var urld="";//Direccion de la aplicacion en heroku
+var url ="http://127.0.0.1:5000/"; //URL donde encadenamos la confirmación de usuario
+var urld="https://miUrldelUNOdeHeroku";
 
-module.exports.enviarEmailConfirmacion=function(direccion,key,msg){
+module.exports.enviarEmailConfirmacion=function(direccion,key){
+    //Construir el Email según quiere sendgrid
+    const msg = {
+        to: 'test@example.com', // Change to your recipient
+        from: 'test@example.com', // Change to your verified sender
+        subject: 'Unocartas: confirmación de correo',
+        text: 'Haga click en el siguiente enlace para confirmar la cuenta',
+        html: '<p><a href="' + url + 'confirmarUsuario/'+direccion+'/'+key+'"> Haz click aquí para confirmar la cuenta</a></p>',}
+      
+      sgMail
+        .send(msg)
+        .then((response) => {
+          console.log(response[0].statusCode)
+          console.log(response[0].headers)
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+}
 
-	const msg = {
-	  to: direccion, // Change to your recipient
-	  from: 'juegocartas@exmaple', //Cuenta profesional del juego uno Change to your verified sender
-	  subject: 'JuegoUno: confirmacion de correo',
-	  text: 'Haga click para confirmar su cuenta',
-	  html: '<p><a href="'+url+'confirmarUsuario/'+direccion+'/'+key+'">Haz click aqui en este enlace para confirmar tu cuenta</a></p>',
-	}
 
-	sgMail
-	  .send(msg)
-	  .then((response) => {
-	    console.log(response[0].statusCode)
-	    console.log(response[0].headers)
-	  })
-	  .catch((error) => {
-	    console.error(error)
-	  })
-  }
+
